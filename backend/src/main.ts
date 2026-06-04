0961b129bc73c00a82d1d2af0d766cb97144db29
Mayank-Choudhary20 <mayankchoudhari123@gmail.com>
2026-06-04 10:00:00 +0530

    New files added name backend/src/experience/experience.controller.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,15 +12,18 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(4000);
 
-  console.log(
-    '🚀 Server running on http://localhost:4000',
-  );
+  console.log('🚀 Server running on http://localhost:4000');
 }
 
 bootstrap();
