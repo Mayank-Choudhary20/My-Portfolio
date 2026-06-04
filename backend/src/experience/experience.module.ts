c7693e602cb129ba99950d7701aa32ad2d8b808f
Mayank-Choudhary20 <mayankchoudhari123@gmail.com>
2026-06-04 10:00:00 +0530

    New files added name backend/src/experience/experience.controller.ts

--- backend/src/experience/experience.module.ts
+++ backend/src/experience/experience.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { ExperienceController } from './experience.controller';
 import { ExperienceService } from './experience.service';
-
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ExperienceController],
   providers: [ExperienceService],
+  exports: [ExperienceService],
 })
 export class ExperienceModule {}
