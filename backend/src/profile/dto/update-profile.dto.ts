4ff4af35b54a9d94e8822c1acb1ce18b511d45d7
Mayank-Choudhary20 <mayankchoudhari123@gmail.com>
2026-06-04 10:00:00 +0530

    New files added name backend/src/experience/experience.controller.ts

--- backend/src/profile/dto/update-profile.dto.ts
+++ backend/src/profile/dto/update-profile.dto.ts
@@ -1,4 +1,75 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProfileDto } from './create-profile.dto';
+import { IsString, IsEmail, IsNumber, IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateProfileDto extends PartialType(CreateProfileDto) {}
+export class UpdateProfileDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  tagline?: string;
+
+  @IsOptional()
+  @IsString()
+  about?: string;
+
+  @IsOptional()
+  @IsString()
+  mission?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  location?: string;
+
+  @IsOptional()
+  @IsString()
+  profileImage?: string;
+
+  @IsOptional()
+  @IsString()
+  github?: string;
+
+  @IsOptional()
+  @IsString()
+  linkedin?: string;
+
+  @IsOptional()
+  @IsString()
+  leetcode?: string;
+
+  @IsOptional()
+  @IsString()
+  codechef?: string;
+
+  @IsOptional()
+  @IsString()
+  codeforces?: string;
+
+  @IsOptional()
+  @IsString()
+  twitter?: string;
+
+  @IsOptional()
+  @IsString()
+  instagram?: string;
+
+  @IsOptional()
+  @IsNumber()
+  yearsExperience?: number;
+
+  @IsOptional()
+  @IsBoolean()
+  available?: boolean;
+}
