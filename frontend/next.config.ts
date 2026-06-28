93d2c0552d78cecfab633e39e1304190a5b63a35
Mayank-Choudhary20 <mayankchoudhari123@gmail.com>
2026-06-28 10:00:00 +0530

    New files added namenewfile:frontend/next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.cloudinary.com",
+      },
+    ],
   },
 };
 
